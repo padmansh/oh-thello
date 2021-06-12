1779b55c047f37a06e3cb59e23a341bab1f36bd8
Aniket Padmansh <pudmaansh@gmail.com>
2021-06-12 15:43:37 +0530

    --fix : pre coin width

--- src/Othello/PreCoin.js
+++ src/Othello/PreCoin.js
@@ -4,7 +4,7 @@ const PreCoin = () => {
   return (
     <>
       <div
-        className={`h-2 w-2 sm:h-3 sm:w-3 rounded-full border-2 border-board_green_dark `}
+        className={`h-3 w-3 rounded-full border-2 border-board_green_dark `}
       />
     </>
   );
